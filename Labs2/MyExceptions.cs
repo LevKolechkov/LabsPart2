62e786cd6e0d65dfb5390da81667f37158f8a318
LevKolechkov <levkolechkov@gmail.com>
2023-04-28 21:23:28 +0700

    Лабораторная №6

---
+++ Labs2/MyExceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project2
+{
+  public class MatrixException : Exception
+  {
+    public MatrixException() { }
+    public MatrixException(string message) : base(message) { }
+    public MatrixException(string message, Exception innerEception) : base(message, innerEception) { }
+  }
+
+  public class ObjectException : Exception
+  {
+    public ObjectException() { }
+    public ObjectException(string message) : base(message) { }
+    public ObjectException(string message, Exception innerEception) : base(message, innerEception) { }
+  }
+}
