f2ae6b036aa3c696cf5a9459bbc07c1e52b2b5c4
LevKolechkov <levkolechkov@gmail.com>
2023-05-23 21:00:03 +0700

    Начало работы над второй лабораторной. Создано создание дерева

---
+++ Labs2/BinaryTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs2
+{
+  public class BinaryTree
+  {
+    public Node Root { get; set; }
+    public Node Current { get; set; }
+
+    private readonly List<int> _listOfNumbers;
+
+    public BinaryTree(List<int> listOfNumbers)
+    {
+      _listOfNumbers = listOfNumbers;
+
+      foreach (int number in _listOfNumbers)
+      {
+        this.Insert(number);
+      }
+    }
+
+    public void Insert (int data)
+    {
+      Root = InsertRecursive(Root, data);
+    }
+
+    public Node InsertRecursive(Node root, int data)
+    {
+      if (root == null)
+      {
+        root = new Node(data);
+        return root;
+      }
+
+      if (data < root.Data)
+      {
+        root.Left = InsertRecursive(root.Left, data);
+      }
+
+      else if (data > root.Data) 
+      {
+        root.Right = InsertRecursive(root.Right, data);
+      }
+
+      return root;
+    }
+
+    public void Next(int fromRootData, int toRootData)
+    {
+
+    }
+  }
+}
---
+++ Labs2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs2
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      List <int> listOfNumbers = new List<int>() {21, 17, 62, 2, 73, 15, 37};
+
+      BinaryTree tree = new BinaryTree(listOfNumbers);
+      
+    }
+  }
+}
